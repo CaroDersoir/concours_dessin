a4f190395c8f1ada3a1d16db2173896fd1224a72
CaroDersoir <145647462+CaroDersoir@users.noreply.github.com>
2026-05-22 23:13:25 +0200

    maj 22/05/2026

--- Service/BP_connection.js
+++
@@ -1,8 +0,0 @@
-// header.js
-
-const loginBtn = document.getElementById("loginBtn");
-
-loginBtn.addEventListener("click", () => {
-    // Redirection vers la page de login
-    window.location.href = "login.html";
-});
